f89dd0729d366ff2661d7bc0500f56c6bec8e7ff
yihwan <hi@yihwan.kim>
2020-09-18 12:39:05 -0700

    lol

--- src/components/commandLineInterface/commandLine/helpModule/index.js
+++ src/components/commandLineInterface/commandLine/helpModule/index.js
@@ -7,7 +7,7 @@ import { HelpContainer, HelpHeader, HelpContent } from './style';
 const HelpModule = () => (
   <HelpContainer>
     <HelpHeader>Page Navigation</HelpHeader>
-    {Object.keys(PAGE_URLS).map(pageNae => <HelpContent>{pageNae}</HelpContent>)}
+    {Object.keys(PAGE_URLS).map(pageName => <HelpContent>{pageName}</HelpContent>)}
 
     <HelpHeader>Editing</HelpHeader>
     <HelpContent>clear - clears terminal screen</HelpContent>
